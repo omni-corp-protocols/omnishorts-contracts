5f0c20cd63915c0b14c99ebf85c09b013fe031da
Leon Nash <leon@ocp.finance>
2021-05-26 10:11:36 +0530

    Updated governance token details

--- contracts/Comptroller.sol
+++ contracts/Comptroller.sol
@@ -1376,6 +1376,6 @@ contract Comptroller is ComptrollerV3Storage, ComptrollerInterface, ComptrollerE
      * @return The address of COMP
      */
     function getCompAddress() public view returns (address) {
-        return 0x3dEB1119c295558c732a3618F04518b9812EC87A;
+        return 0x10826e17f9Fa5b5606792c9954b4a36193782b2d;
     }
 }
--- contracts/Governance/Comp.sol
+++ contracts/Governance/Comp.sol
@@ -3,16 +3,16 @@ pragma experimental ABIEncoderV2;
 
 contract Comp {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Omni COMP Governance Token";
+    string public constant name = "Omni Shorts";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "OMNIC";
+    string public constant symbol = "OMNIS";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
 
     /// @notice Total number of tokens in circulation
-    uint public constant totalSupply = 100000000e18; // 100 million OMNIC
+    uint public constant totalSupply = 100000000e18; // 100 million OMNIS
 
     /// @notice Allowance amounts on behalf of others
     mapping (address => mapping (address => uint96)) internal allowances;
